17bade94e11eb6c5096facd9976de22a9f09eba8
Panchajanya1999 <rsk52959@gmail.com>
2025-02-22 18:52:41 +0530

    fw/plugins/linux: elfs: Apply proper fix to skip certain reserved addresses
    This will help skipping certain reserved addresses which is smeared
    due to system memory acquisition. On Android Devices, these mem blocks
    are generally reserved and no apps are allowed to break through them,
    exactly what TEE does.
    
    The most effective way is to skip these ranges and continue the acquisistion.
    
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- volatility3/framework/plugins/linux/elfs.py
+++ volatility3/framework/plugins/linux/elfs.py
@@ -15,11 +15,10 @@ from volatility3.framework.renderers import format_hints
 from volatility3.framework.symbols import intermed
 from volatility3.framework.symbols.linux.extensions import elf
 from volatility3.framework.constants.linux import ELF_MAX_EXTRACTION_SIZE
+from volatility3.framework.exceptions import InvalidAddressException
 from volatility3.plugins.linux import pslist
 
-
 vollog = logging.getLogger(__name__)
-
 
 class Elfs(plugins.PluginInterface):
     """Lists all memory mapped ELF files for all processes."""
@@ -73,114 +72,123 @@ class Elfs(plugins.PluginInterface):
         Returns:
             An open FileHandlerInterface object containing the complete data for the task or None in the case of failure
         """
-
         proc_layer = context.layers[layer_name]
         file_handle = None
 
-        elf_object = context.object(
-            elf_table_name + constants.BANG + "Elf",
-            offset=vma.vm_start,
-            layer_name=layer_name,
-        )
+        try:
+            elf_object = context.object(
+                elf_table_name + constants.BANG + "Elf",
+                offset=vma.vm_start,
+                layer_name=layer_name,
+            )
 
-        if not elf_object.is_valid():
-            vollog.debug("ELF object to be dumped is not valid")
-            return None
+            if not elf_object.is_valid():
+                vollog.debug(f"ELF object at {vma.vm_start:#x} for PID {task.pid} is not valid")
+                return None
 
-        sections = {}
-        # TODO: Apply more effort to reconstruct ELF, e.g.: https://github.com/enbarberis/core2ELF64 ?
-        for phdr in elf_object.get_program_headers():
-            try:
-                if phdr.p_type.description != "PT_LOAD":
+            sections = {}
+            for phdr in elf_object.get_program_headers():
+                try:
+                    if phdr.p_type.description != "PT_LOAD":
+                        continue
+                except ValueError:
+                    vollog.log(
+                        constants.LOGLEVEL_VVVV,
+                        f"Skipping unknown ELF program header type: {phdr.p_type} at {vma.vm_start:#x}",
+                    )
                     continue
-            except ValueError:
-                vollog.log(
-                    constants.LOGLEVEL_VVVV,
-                    f"Skipping unknown ELF program header type: {phdr.p_type}",
-                )
-                continue
 
-            start = phdr.p_vaddr
-            size = phdr.p_memsz
-            end = start + size
+                start = phdr.p_vaddr
+                size = phdr.p_memsz
+                end = start + size
 
-            # Use complete memory pages for dumping
-            # If start isn't a multiple of a page, stick to the highest multiple < start
-            # If end isn't a multiple of a page, stick to the lowest multiple > end
-            if start % proc_layer.page_size:
-                start = start & proc_layer.page_mask
+                if start % proc_layer.page_size:
+                    start = start & proc_layer.page_mask
+                if end % proc_layer.page_size:
+                    end = (end & proc_layer.page_mask) + proc_layer.page_size
 
-            if end % proc_layer.page_size:
-                end = (end & proc_layer.page_mask) + proc_layer.page_size
+                real_size = end - start
+                if real_size < 0 or real_size > ELF_MAX_EXTRACTION_SIZE:
+                    vollog.debug(f"Invalid ELF size {real_size} at {vma.vm_start:#x} for PID {task.pid}")
+                    return None
 
-            real_size = end - start
+                sections[start] = real_size
 
-            # Check if ELF has a legitimate size
-            if real_size < 0 or real_size > ELF_MAX_EXTRACTION_SIZE:
-                raise ValueError(f"The claimed size of the ELF is invalid: {real_size}")
+            elf_data = b""
+            for section_start in sorted(sections.keys()):
+                read_size = sections[section_start]
+                try:
+                    buf = proc_layer.read(vma.vm_start + section_start, read_size, pad=True)
+                    elf_data += buf
+                except InvalidAddressException as e:
+                    vollog.warning(f"Failed to read ELF section at {vma.vm_start + section_start:#x}: {e}")
+                    return None
 
-            sections[start] = real_size
+            file_handle = open_method(
+                f"pid.{task.pid}.{utility.array_to_string(task.comm)}.{vma.vm_start:#x}.dmp"
+            )
+            file_handle.write(elf_data)
+            return file_handle
 
-        elf_data = b""
-        for section_start in sorted(sections.keys()):
-            read_size = sections[section_start]
-
-            buf = proc_layer.read(vma.vm_start + section_start, read_size, pad=True)
-            elf_data = elf_data + buf
-
-        file_handle = open_method(
-            f"pid.{task.pid}.{utility.array_to_string(task.comm)}.{vma.vm_start:#x}.dmp"
-        )
-        file_handle.write(elf_data)
-
-        return file_handle
+        except InvalidAddressException as e:
+            vollog.warning(f"Skipping ELF dump for PID {task.pid} at {vma.vm_start:#x}: {e}")
+            return None
 
     def _generator(self, tasks):
         elf_table_name = intermed.IntermediateSymbolTable.create(
             self.context, self.config_path, "linux", "elf", class_types=elf.class_types
         )
         for task in tasks:
-            proc_layer_name = task.add_process_layer()
-            if not proc_layer_name:
-                continue
-
-            proc_layer = self.context.layers[proc_layer_name]
-
-            name = utility.array_to_string(task.comm)
-
-            for vma in task.mm.get_vma_iter():
-                hdr = proc_layer.read(vma.vm_start, 4, pad=True)
-                if hdr != b"\x7fELF":
+            try:
+                proc_layer_name = task.add_process_layer()
+                if not proc_layer_name:
+                    vollog.debug(f"No process layer for PID {task.pid}")
                     continue
 
-                path = vma.get_name(self.context, task)
+                proc_layer = self.context.layers[proc_layer_name]
+                name = utility.array_to_string(task.comm)
 
-                file_output = "Disabled"
-                if self.config["dump"]:
-                    file_handle = self.elf_dump(
-                        self.context,
-                        proc_layer_name,
-                        elf_table_name,
-                        vma,
-                        task,
-                        self.open,
-                    )
-                    file_output = "Error outputting file"
-                    if file_handle:
-                        file_handle.close()
-                        file_output = str(file_handle.preferred_filename)
+                for vma in task.mm.get_vma_iter():
+                    try:
+                        hdr = proc_layer.read(vma.vm_start, 4, pad=True)
+                        if hdr != b"\x7fELF":
+                            continue
 
-                yield (
-                    0,
-                    (
-                        task.pid,
-                        name,
-                        format_hints.Hex(vma.vm_start),
-                        format_hints.Hex(vma.vm_end),
-                        path,
-                        file_output,
-                    ),
-                )
+                        path = vma.get_name(self.context, task)
+
+                        file_output = "Disabled"
+                        if self.config["dump"]:
+                            file_handle = self.elf_dump(
+                                self.context,
+                                proc_layer_name,
+                                elf_table_name,
+                                vma,
+                                task,
+                                self.open,
+                            )
+                            file_output = "Error outputting file"
+                            if file_handle:
+                                file_handle.close()
+                                file_output = str(file_handle.preferred_filename)
+
+                        yield (
+                            0,
+                            (
+                                task.pid,
+                                name,
+                                format_hints.Hex(vma.vm_start),
+                                format_hints.Hex(vma.vm_end),
+                                path,
+                                file_output,
+                            ),
+                        )
+                    except InvalidAddressException as e:
+                        vollog.warning(f"Skipping VMA at {vma.vm_start:#x} for PID {task.pid} ({name}): {e}")
+                        continue
+
+            except InvalidAddressException as e:
+                vollog.warning(f"Skipping task PID {task.pid} ({name}): {e}")
+                continue
 
     def run(self):
         filter_func = pslist.PsList.create_pid_filter(self.config.get("pid", None))
